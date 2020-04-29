be61e5f6bde3c3b299f7718c575797367123e794
837910164 <2098055@163.com>
2020-04-30 01:14:25 +0800

    pull一下

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,7 @@ const store = new Vuex.Store({
         },
         love: [],//收藏的商品
         cart: [],//购物车商品
+        addressList:[],//地址
         //...
     },
     getters: {
@@ -25,6 +26,9 @@ const store = new Vuex.Store({
         Cart: state => {
             return state.cart;
         },
+        AddressList: state=>{
+            return state.addressList;
+        }
 
         //...
     },
@@ -37,6 +41,10 @@ const store = new Vuex.Store({
         getMyLove(state,data) {
             state.love = data;
             console.log(state.love)
+        },
+        getAddresses(state,data){
+            state.addressList = data;
+            console.log(state.addressList)
         },
 
         //将商品添加入购物车，目前只是简单实现未登录的添加
@@ -62,6 +70,7 @@ const store = new Vuex.Store({
                 email: "",
                 love: [],
                 cart: [],
+                addressList:[],
             };
         }
         //...
@@ -74,6 +83,33 @@ const store = new Vuex.Store({
         async getMyLove(context) {
             let res = await api.get('/collections');
             context.commit('getMyLove', res.data.data);
+        },
+        async getAddresses(context) {
+            let data = await api.get("/addresses");
+            console.log(data);
+            if (data.status >= 200 && data.status < 300) {
+                data = data.data;
+                var tempList = [];
+                for (var i = 0; i < data.length; i++) {
+                    var address = '';
+                    if (data[i].province == data[i].city)  //如果省份和城市是一样的，例如上海市，北京市，address避免重复
+                        address = data[i].province + ' ' + data[i].county + ' ' + data[i].detail;
+                    else
+                        address = data[i].province + ' ' + data[i].city + ' ' + data[i].county + ' ' + data[i].detail;
+                    tempList.push({
+                        id: data[i].id,
+                        name: data[i].name,
+                        tel: data[i].phone,
+                        province: data[i].province,
+                        city: data[i].city,
+                        county: data[i].county,
+                        addressDetail: data[i].detail,
+                        areaCode: data[i].code,
+                        address: address
+                    });
+                }
+                context.commit('getAddresses',tempList);
+            }
         },
 
     }
